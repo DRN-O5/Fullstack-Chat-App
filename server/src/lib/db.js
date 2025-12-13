ad0a1ad27ca46c5daf25665fa4d227cdffbc5b11
DRN-O5 <dibyaranjan1805@gmail.com>
2025-12-13 18:39:50 +0530

    Initial Commit - Project Completed

---
+++ server/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Connected to MongoDB: ${mongoose.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+};
