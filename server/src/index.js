2b754cbe513dc0e4d14fdf4d67bca6b4c85afcdd
DRN-O5 <dibyaranjan1805@gmail.com>
2025-12-13 19:17:54 +0530

    Fixed Depolyment issue 2

--- server/src/index.js
+++ server/src/index.js
@@ -31,9 +31,9 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  })
+  });
 }
 
 app.get("/", (req, res) => {
